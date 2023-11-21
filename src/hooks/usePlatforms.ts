414f22736049e542764469d30a01cbe8c2721aae
Ter-web <n.terfa@yahoo.com>
2023-11-21 22:47:57 +0200

    Refactoring-create lookup hooks

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,7 +2,7 @@
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
-import  { FetchResponse } from "../services/api-client";
+
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents')
 export interface Platform {
@@ -18,7 +18,7 @@ const usePlatforms = () => useQuery({
       queryKey: ['platforms'],
       queryFn: apiClient.getAll,
       staleTime: 24 * 60 * 60 * 1000,
-      initialData: {count: platforms.length, results: platforms}
+      initialData: platforms
 })
 
 export default usePlatforms;

28f75342a7b4bf196a4d98c4e5696c37b6a40053
Ter-web <n.terfa@yahoo.com>
2023-11-30 18:20:12 +0200

    adjusting

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-      <NavBar margin-bottom={5} />
+      <NavBar />
       <Box p={5}>
         <Outlet />
       </Box>
